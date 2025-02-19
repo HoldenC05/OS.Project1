5985f746f0dfbee2c9cdce7fab053d93d230f59e
Holden Caldwell <hwc2578@utulsa.edu>
2025-02-19 14:53:09 -0600

    add sections and finish section - ready to merge

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,7 +7,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die(const char *s) {
     perror(s);
@@ -31,6 +36,7 @@ void enableRawMode() {
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+/*** init ***/
 
 int main() {
     enableRawMode();
