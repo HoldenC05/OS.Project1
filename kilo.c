bdd6287e63ffc81c09ea17602617c3eebcd0ffc6
Holden Caldwell <hwc2578@utulsa.edu>
2025-02-20 17:13:33 -0600

    multiple lines of text work now

--- kilo.c
+++ kilo.c
@@ -43,7 +43,7 @@ struct editorConfig {
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -165,6 +165,18 @@ int getWindowSize (int *rows, int *cols) {
     }
 }
 
+/*** row operations ***/
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+  }
+
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -172,22 +184,17 @@ void editorOpen(char *filename) {
     if (!fp) die("fopen");
 
     char *line = NULL;
-    size_t linecap =0;
-    ssize_t linelen = 13;
-    linelen = getline(&line, &linecap, fp);
-    if (linelen != -1){
+    size_t linecap = 0;
+    ssize_t linelen;
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
         while (linelen > 0 && (line[linelen - 1] == '\n' ||
                                line[linelen - 1] == '\r'))
-            linelen--;
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+        linelen--;
+      editorAppendRow(line, linelen);
+    }
+    free(line);
+    fclose(fp);
   }
-  free(line);
-  fclose(fp);
-}
 
 /*** append buffer ***/
 
@@ -234,9 +241,9 @@ void editorDrawRows(struct abuf *ab) {
                     abAppend(ab, "~", 1);
             }
             } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
     
 
@@ -335,6 +342,7 @@ void initEditor(void) {
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
