194e2de3733662d2ddf8abd786ca4e0653a20b7d
Holden Caldwell <hwc2578@utulsa.edu>
2025-02-20 23:07:42 -0600

    Fixed save function

--- kilo.c
+++ kilo.c
@@ -364,36 +364,38 @@ void editorOpen(char *filename) {
     E.dirty = 0;
   }
 
-void editorSave(void) {
-        if (E.filename == NULL) {
-            E.filename = editorPrompt("Save as: %s (ESC to cancel)");
-            if (E.filename == NULL) {
-                editorSetStatusMessage("Save aborted");
-                return;
-            }
-
+void editorSave(void)
+{
+    if (E.filename == NULL)
+    {
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL)
+        {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
     int len;
     char *buf = editorRowsToString(&len);
-
     int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-    if (fd != -1) {
-        if (ftruncate(fd, len) != -1) {
-            if (write(fd, buf, len) == len) {
+    if (fd != -1)
+    {
+        if (ftruncate(fd, len) != -1)
+        {
+            if (write(fd, buf, len) == len)
+            {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editorSetStatusMessage("%d bytes written to disk", len);
                 return;
-          }
+            }
         }
         close(fd);
-      }
-        free(buf);
-        E.dirty = 0;
-        editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
-
-  }
+    }
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
-  
 
 /*** append buffer ***/
 
