bd02ef8db980f92e19fc2ba67f9c5c1adae94d7a
Holden Caldwell <hwc2578@utulsa.edu>
2025-02-20 21:35:25 -0600

    NOW A TEXT EDITOR! Inserting basic characters is implemented.

--- kilo.c
+++ kilo.c
@@ -227,6 +227,25 @@ void editorAppendRow(char *s, size_t len) {
     E.numrows++;
   }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+  }
+  
+/*** editor operations ***/
+
+void editorInsertChar(int c)
+{
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
 
 /*** file i/o ***/
 
@@ -431,7 +450,6 @@ void editorMoveCursor(int key) {
         E.cx = rowlen;
     }  
   }
-  
 
 void editorProcessKeypress(void) {
     int c = editorReadKey();
@@ -473,6 +491,10 @@ void editorProcessKeypress(void) {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+
+            default:
+                editorInsertChar(c);
+                break;
     }
 }
 
