3b088783e67f51d83b62e0f4d71deca15ee67de4
Holden Caldwell <hwc2578@utulsa.edu>
2025-02-20 21:42:04 -0600

    Mapped keys

--- kilo.c
+++ kilo.c
@@ -23,6 +23,7 @@
 #define KILO_TAB_STOP 8
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -455,6 +456,10 @@ void editorProcessKeypress(void) {
     int c = editorReadKey();
 
     switch (c) {
+        case '\r':
+            /*TODO*/
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -468,6 +473,12 @@ void editorProcessKeypress(void) {
         case END_KEY:
             if (E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
+
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /*TODO*/
+            break;
 
         case PAGE_UP:
         case PAGE_DOWN:
@@ -490,6 +501,10 @@ void editorProcessKeypress(void) {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+
+        case CTRL_KEY('l'):
+        case '\x1b':
             break;
 
             default:
